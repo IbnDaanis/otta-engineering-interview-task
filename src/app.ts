0578806f081d77fb05ee64b8be96d59bea06545b
Ikraam <IbnDaanis@Gmail.com>
2021-05-08 21:44:26 +0100

    Minor refactor

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import csv from 'csvtojson/v2'
+// import csv from 'csvtojson/v2'
 
 interface ReactionInterface {
   user_id: string
@@ -12,28 +12,28 @@ interface UsersInterface {
 }
 
 const main = async () => {
-  const csvFilePath = 'src/data/reactions.csv'
-  const data: ReactionInterface[] = await csv().fromFile(csvFilePath)
-  const reactions = data
-    .filter(reaction => reaction.direction === 'true')
-    .sort((a, b) => +b.job_id - +a.job_id)
+  // const csvFilePath = 'src/data/reactions.csv'
+  // const data: ReactionInterface[] = await csv().fromFile(csvFilePath)
+  // const reactions = data
+  //   .filter(reaction => reaction.direction === 'true')
+  //   .sort((a, b) => +b.job_id - +a.job_id)
 
-  // const reactions = [
-  //   { job_id: 'microsoft', user_id: 'john', direction: 'true' },
-  //   { job_id: 'microsoft', user_id: 'jack', direction: 'true' },
-  //   { job_id: 'apple', user_id: 'john', direction: 'true' },
-  //   { job_id: 'amazon', user_id: 'john', direction: 'true' },
-  //   { job_id: 'apple', user_id: 'james', direction: 'true' },
-  //   { job_id: 'google', user_id: 'jane', direction: 'true' },
-  //   { job_id: 'apple', user_id: 'mark', direction: 'true' },
-  //   { job_id: 'amazon', user_id: 'jane', direction: 'true' },
-  //   { job_id: 'google', user_id: 'mark', direction: 'true' },
-  //   { job_id: 'google', user_id: 'john', direction: 'true' },
-  //   { job_id: 'apple', user_id: 'joseph', direction: 'true' },
-  //   { job_id: 'microsoft', user_id: 'joseph', direction: 'true' },
-  //   { job_id: 'netflix', user_id: 'jane', direction: 'true' },
-  //   { job_id: 'apple', user_id: 'jane', direction: 'true' }
-  // ]
+  const reactions: ReactionInterface[] = [
+    { job_id: 'microsoft', user_id: 'john', direction: 'true', time: '' },
+    { job_id: 'microsoft', user_id: 'jack', direction: 'true', time: '' },
+    { job_id: 'apple', user_id: 'john', direction: 'true', time: '' },
+    { job_id: 'amazon', user_id: 'john', direction: 'true', time: '' },
+    { job_id: 'apple', user_id: 'james', direction: 'true', time: '' },
+    { job_id: 'google', user_id: 'jane', direction: 'true', time: '' },
+    { job_id: 'apple', user_id: 'mark', direction: 'true', time: '' },
+    { job_id: 'amazon', user_id: 'jane', direction: 'true', time: '' },
+    { job_id: 'google', user_id: 'mark', direction: 'true', time: '' },
+    { job_id: 'google', user_id: 'john', direction: 'true', time: '' },
+    { job_id: 'apple', user_id: 'joseph', direction: 'true', time: '' },
+    { job_id: 'microsoft', user_id: 'joseph', direction: 'true', time: '' },
+    { job_id: 'netflix', user_id: 'jane', direction: 'true', time: '' },
+    { job_id: 'apple', user_id: 'jane', direction: 'true', time: '' }
+  ]
 
   const users = reactions.reduce(
     (accumulator: UsersInterface, current: ReactionInterface): UsersInterface => {
@@ -50,13 +50,11 @@ const main = async () => {
 
   const likes: [string, Set<string>][] = Object.entries(users).sort((a, b) => b[1].size - a[1].size)
 
-  for (let i = 0; i < likes.length; i++) {
-    if (i > 0) {
-      const curr = likes[i]
-      const prev = likes[i - 1]
-      if (curr[1].size === prev[1].size) {
-        console.log('Match: ', curr[0], prev[0], curr[1].size)
-      }
+  for (let i = 1; i < likes.length - 1; i++) {
+    const curr = likes[i]
+    const prev = likes[i - 1]
+    if (curr[1].size === prev[1].size) {
+      console.log('Match: ', curr[0], prev[0], curr[1].size)
     }
   }
   return users
