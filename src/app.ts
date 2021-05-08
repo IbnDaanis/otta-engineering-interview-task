20eca4ba3a51003f0c62c93cffab21c6bed20f47
Ikraam <IbnDaanis@Gmail.com>
2021-05-08 03:51:15 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,85 +1,91 @@
-import csv from 'csvtojson/v2'
+// import csv from 'csvtojson/v2'
 
-interface ReactionInterface {
-  user_id: string
-  job_id: string
-  direction: string
-  time: string
-}
+// interface ReactionInterface {
+//   user_id: string
+//   job_id: string
+//   direction: string
+//   time: string
+// }
 
-interface JobsInterface {
-  [key: string]: {
-    users: string[]
-  }
-}
+// interface JobsInterface {
+//   [key: string]: {
+//     users: string[]
+//   }
+// }
 
-interface userSimilarityInterface {
-  [key: string]: {
-    user: string
-    count: number
-  }
-}
+// interface userSimilarityInterface {
+//   [key: string]: {
+//     user: string
+//     count: number
+//   }
+// }
 
 const main = async () => {
-  const csvFilePath = 'src/data/reactions.csv'
-  const data: ReactionInterface[] = await csv().fromFile(csvFilePath)
-  const reactions = data.filter(reaction => reaction.direction === 'true')
+  // const csvFilePath = 'src/data/reactions.csv'
+  // const data: ReactionInterface[] = await csv().fromFile(csvFilePath)
+  // const reactions = data.filter(reaction => reaction.direction === 'true').sort((a,b)=> +b.job_id - +a.job_id)
 
-  const users = [...new Set(reactions.map(reaction => reaction.user_id))]
-  const jobs: JobsInterface = {}
+  // const reactions = [
+  //   { job_id: 'apple', user_id: 'john', direction: true },
+  //   { job_id: 'google', user_id: 'jane', direction: true },
+  //   { job_id: 'amazon', user_id: 'john', direction: true },
+  //   { job_id: 'apple', user_id: 'jane', direction: true },
+  //   { job_id: 'google', user_id: 'john', direction: true },
+  //   { job_id: 'apple', user_id: 'mark', direction: true },
+  //   { job_id: 'amazon', user_id: 'jane', direction: true },
+  //   { job_id: 'google', user_id: 'mark', direction: true },
+  //   { job_id: 'microsoft', user_id: 'john', direction: true },
+  //   { job_id: 'apple', user_id: 'james', direction: true }
+  // ]
 
-  for (let i = 0; i < reactions.length; i++) {
-    // Add jobs and the users who liked them
-    const currentReaction = reactions[i]
-    const currentJob = jobs[currentReaction.job_id]
-    if (jobs[currentReaction.job_id]) {
-      jobs[currentReaction.job_id] = {
-        users: [...currentJob.users, currentReaction.user_id]
+  const reactions = [
+    { job_id: 12, user_id: 'john', direction: true },
+    { job_id: 2, user_id: 'jane', direction: true },
+    { job_id: 3, user_id: 'john', direction: true },
+    { job_id: 4, user_id: 'jane', direction: true },
+    { job_id: 5, user_id: 'john', direction: true },
+    { job_id: 6, user_id: 'mark', direction: true },
+    { job_id: 7, user_id: 'jane', direction: true },
+    { job_id: 8, user_id: 'mark', direction: true },
+    { job_id: 9, user_id: 'john', direction: true },
+    { job_id: 10, user_id: 'james', direction: true }
+  ]
+
+  const users = reactions.reduce(
+    (acc: any, r): any => {
+      if (!acc[r.user_id]) {
+        acc[r.user_id] = new Set()
+        if (r.direction) acc[r.user_id].add(r.job_id)
+      } else if (r.direction) {
+        acc[r.user_id].add(r.job_id)
       }
-    } else {
-      jobs[currentReaction.job_id] = {
-        users: [currentReaction.user_id]
+
+      const key: any = Array.from(acc[r.user_id]).reduce((acc: any, curr: any) => +acc * +curr, 1)
+
+      if (!acc.jobs[key]) {
+        acc.jobs[key] = new Set([r.user_id])
+      } else {
+        acc.jobs[key].add(r.user_id)
       }
-    }
-  }
 
-  for (let i = 0; i < reactions.length; i++) {
-    // Remove jobs that only have one person who liked them and sort users
-    const currentReaction = reactions[i]
-    const currentJob = jobs[currentReaction.job_id]
-    if (!currentJob?.users || currentJob.users.length < 2) {
-      delete jobs[currentReaction.job_id]
-    }
-    if (jobs[currentReaction.job_id]) {
-      jobs[currentReaction.job_id].users = [
-        ...new Set(jobs[currentReaction.job_id].users.sort((a, b) => +a - +b))
-      ]
-    }
-  }
-
-  const userSimilarity: userSimilarityInterface = {}
-
-  for (let i = 0; i < users.length; i++) {
-    // if (users.length === 134) {
-    //   console.log(users)
-    // }
-    // console.log(users[i])
-    //     userSimilarity[users[i]] = {}
-
-    for (let j = 0; j < users.length; j++) {
-      if (users[i] === users[j]) continue
-      let count = 0
-      for (const job in jobs) {
-        if (jobs[job].users.includes(users[i]) && jobs[job].users.includes(users[j])) {
-          count++
-        }
-      }
-      if (!count) continue
-      userSimilarity[users[i]] = { user: users[j], count }
-    }
-  }
-  // console.log(userSimilarity)
-  console.log(Object.entries(userSimilarity).sort((a, b) => b[1].count - a[1].count)[0])
+      return acc
+    },
+    { jobs: {} }
+  )
+  // console.log(Object.entries(users.jobs).sort((a, b) => b[0].length - a[0].length))
+  console.log(users)
+  return users
 }
 
 main()
+
+// const reactions = [
+//   { jobId: 'apple', userId: 'john', direction: true },
+//   { jobId: 'apple', userId: 'jane', direction: true },
+//   { jobId: 'apple', userId: 'mark', direction: true },
+//   { jobId: 'amazon', userId: 'john', direction: true },
+//   { jobId: 'amazon', userId: 'jane', direction: true },
+//   { jobId: 'google', userId: 'john', direction: true },
+//   { jobId: 'google', userId: 'jane', direction: true },
+//   { jobId: 'google', userId: 'mark', direction: true }
+// ]
