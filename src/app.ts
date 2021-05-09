9b2724529fda9406f269e6fcff112841883cd549
Ikraam <IbnDaanis@Gmail.com>
2021-05-09 23:09:57 +0100

    Task 1

--- src/app.ts
+++ src/app.ts
@@ -19,24 +19,24 @@ const main = async () => {
     .sort((a, b) => +b.job_id - +a.job_id)
 
   // const reactions: ReactionInterface[] = [
-  //   { job_id: 'microsoft', user_id: 'john', direction: 'true', time: '' },
-  //   { job_id: 'microsoft', user_id: 'jack', direction: 'true', time: '' },
+  //   { job_id: 'facebook', user_id: 'john', direction: 'true', time: '' },
   //   { job_id: 'apple', user_id: 'john', direction: 'true', time: '' },
   //   { job_id: 'amazon', user_id: 'john', direction: 'true', time: '' },
-  //   { job_id: 'apple', user_id: 'james', direction: 'true', time: '' },
-  //   { job_id: 'google', user_id: 'jane', direction: 'true', time: '' },
-  //   { job_id: 'apple', user_id: 'mark', direction: 'true', time: '' },
-  //   { job_id: 'amazon', user_id: 'jane', direction: 'true', time: '' },
-  //   { job_id: 'google', user_id: 'mark', direction: 'true', time: '' },
+  //   { job_id: 'netflix', user_id: 'john', direction: 'true', time: '' },
   //   { job_id: 'google', user_id: 'john', direction: 'true', time: '' },
-  //   { job_id: 'apple', user_id: 'joseph', direction: 'true', time: '' },
-  //   { job_id: 'microsoft', user_id: 'joseph', direction: 'true', time: '' },
+  //   { job_id: 'facebook', user_id: 'jane', direction: 'true', time: '' },
+  //   { job_id: 'apple', user_id: 'jane', direction: 'true', time: '' },
+  //   { job_id: 'amazon', user_id: 'jane', direction: 'true', time: '' },
   //   { job_id: 'netflix', user_id: 'jane', direction: 'true', time: '' },
-  //   { job_id: 'apple', user_id: 'jane', direction: 'true', time: '' }
+  //   { job_id: 'facebook', user_id: 'james', direction: 'true', time: '' },
+  //   { job_id: 'apple', user_id: 'james', direction: 'true', time: '' },
+  //   { job_id: 'amazon', user_id: 'james', direction: 'true', time: '' },
+  //   { job_id: 'facebook', user_id: 'jack', direction: 'true', time: '' },
+  //   { job_id: 'apple', user_id: 'jack', direction: 'true', time: '' }
   // ]
 
-  const users = reactions.reduce(
-    (accumulator: UsersInterface, current: ReactionInterface): UsersInterface => {
+  const users = Object.entries(
+    reactions.reduce((accumulator: UsersInterface, current: ReactionInterface): UsersInterface => {
       if (!accumulator[current.user_id]) {
         accumulator[current.user_id] = new Set()
         if (current.direction) accumulator[current.user_id].add(current.job_id)
@@ -44,30 +44,58 @@ const main = async () => {
         accumulator[current.user_id].add(current.job_id)
       }
       return accumulator
-    },
-    {}
+    }, {})
   )
 
-  const likes: [string, Set<string>][] = Object.entries(users).sort((a, b) => b[1].size - a[1].size)
+  const compareLikes = (first: Set<string>, second: Set<string>): number => {
+    let common = 0
+    for (const a of first) {
+      if (second.has(a)) common++
+    }
+    return common
+  }
 
-  for (let i = 1; i < likes.length; i++) {
-    const curr = likes[i]
-    const prev = likes[i - 1]
-    if (curr[1].size === prev[1].size) {
-      // console.log('Match: ', curr[0], prev[0], curr[1].size)
-      break
+  const answers: any = []
+  let score: number = 0
+
+  for (let i = 0; i < users.length; i++) {
+    const currentUser = users[i]
+
+    if (!answers.length && !score) {
+      answers[0] = currentUser
+      answers[1] = users[i + 1]
+      score = compareLikes(currentUser[1], users[i + 1][1])
+      i++
+      continue
+    }
+
+    const comparisonToFirst: number = compareLikes(currentUser[1], answers[0][1])
+    const comparisonToSecond: number = compareLikes(currentUser[1], answers[1][1])
+
+    if (comparisonToFirst > score) {
+      answers[1] = currentUser
+      score = comparisonToFirst
+      continue
+    }
+
+    if (comparisonToSecond > score) {
+      answers[0] = currentUser
+      score = comparisonToSecond
     }
   }
-  return users
+
+  console.log(answers, score)
+
+  return answers
 }
 
 main()
 
-const jobs = async () => {
-  const csvFilePath = 'src/data/jobs.csv'
-  const jobsData = await csv().fromFile(csvFilePath)
+// const jobs = async () => {
+//   const csvFilePath = 'src/data/jobs.csv'
+//   const jobsData = await csv().fromFile(csvFilePath)
 
-  console.log(jobsData)
-}
+//   console.log(jobsData.sort((a, b) => +b.company_id - +a.company_id))
+// }
 
-jobs()
+// jobs()
