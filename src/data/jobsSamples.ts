084550e3c16f0d0c00b57338d780528bd63c57b6
Ikraam <IbnDaanis@Gmail.com>
2021-05-10 00:29:22 +0100

    samples updated

--- src/data/jobsSamples.ts
+++ src/data/jobsSamples.ts
@@ -1,18 +1,18 @@
 import { JobInterface } from '../interfaces/JobInterface'
 
 export const reactionsSample1: JobInterface[] = [
-  { job_id: 'facebook', company_id: 'facebook' },
-  { job_id: 'apple', company_id: 'apple' },
-  { job_id: 'amazon', company_id: 'amazon' },
-  { job_id: 'netflix', company_id: 'netflix' },
-  { job_id: 'google', company_id: 'google' },
-  { job_id: 'facebook', company_id: 'facebook' },
-  { job_id: 'apple', company_id: 'apple' },
-  { job_id: 'amazon', company_id: 'amazon' },
-  { job_id: 'netflix', company_id: 'netflix' },
-  { job_id: 'facebook', company_id: 'facebook' },
-  { job_id: 'apple', company_id: 'apple' },
-  { job_id: 'amazon', company_id: 'amazon' },
-  { job_id: 'facebook', company_id: 'facebook' },
-  { job_id: 'apple', company_id: 'apple' }
+  { job_id: 'senior facebook', company_id: 'facebook' },
+  { job_id: 'mid apple', company_id: 'apple' },
+  { job_id: 'senior amazon', company_id: 'amazon' },
+  { job_id: 'senior netflix', company_id: 'netflix' },
+  { job_id: 'junior google', company_id: 'google' },
+  { job_id: 'senior facebook', company_id: 'facebook' },
+  { job_id: 'mid apple', company_id: 'apple' },
+  { job_id: 'senior amazon', company_id: 'amazon' },
+  { job_id: 'senior netflix', company_id: 'netflix' },
+  { job_id: 'senior facebook', company_id: 'facebook' },
+  { job_id: 'mid apple', company_id: 'apple' },
+  { job_id: 'senior amazon', company_id: 'amazon' },
+  { job_id: 'senior facebook', company_id: 'facebook' },
+  { job_id: 'mid apple', company_id: 'apple' }
 ]
